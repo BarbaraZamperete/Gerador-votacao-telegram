1d69ee4d94066fac784f60ea24ef997781e4ffab
Bárbara Zamperete <bahzamp25@gmail.com>
2022-07-19 17:37:13 -0400

    REFACTOR: separei dados de teste do codigo em si

---
+++ data.js
@@ -0,0 +1,56 @@
+const votacoes = [
+    {
+        id: 1,
+        titulo: "Decidir coisa X",
+        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
+        inicio: "17/07/2022",
+        termino: "28/07/2022",
+        status: "ativa",
+        sim: 24,
+        nao: 32,
+        total: 56
+    },
+    {
+        id: 2,
+        titulo: "Decidir coisa X",
+        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
+        inicio: "17/07/2022",
+        termino: "28/07/2022",
+        status: "ativa",
+        sim: 24,
+        nao: 32,
+        total: 56
+    },
+    {
+        id: 3,
+        titulo: "Decidir coisa X",
+        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
+        inicio: "17/07/2022",
+        termino: "28/07/2022",
+        status: "ativa",
+        sim: 24,
+        nao: 32,
+        total: 56
+    },
+    {
+        id: 4,
+        titulo: "Decidir coisa X",
+        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
+        inicio: "17/07/2022",
+        termino: "28/07/2022",
+        status: "ativa",
+        sim: 24,
+        nao: 32,
+        total: 56
+    },
+]
+
+const usuarios = [
+    { id: 1, nome: "Fulano", numero: "991292263" },
+    { id: 2, nome: "Fulano", numero: "981292264" },
+    { id: 3, nome: "Fulano", numero: "981293265" },
+    { id: 4, nome: "Fulano", numero: "991293266" },
+]
+const votacao = votacoes[0]
+
+module.exports = {votacoes, usuarios, votacao}
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,61 +3,9 @@ const {
 } = require('express');
 const router = Router();
 
-const votacoes = [
-    {
-        id: 1,
-        titulo: "Decidir coisa X",
-        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
-        inicio: "17/07/2022",
-        termino: "28/07/2022",
-        status: "ativa",
-        sim: 24,
-        nao: 32,
-        total: 56
-    },
-    {
-        id: 2,
-        titulo: "Decidir coisa X",
-        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
-        inicio: "17/07/2022",
-        termino: "28/07/2022",
-        status: "ativa",
-        sim: 24,
-        nao: 32,
-        total: 56
-    },
-    {
-        id: 3,
-        titulo: "Decidir coisa X",
-        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
-        inicio: "17/07/2022",
-        termino: "28/07/2022",
-        status: "ativa",
-        sim: 24,
-        nao: 32,
-        total: 56
-    },
-    {
-        id: 4,
-        titulo: "Decidir coisa X",
-        descricao: "Vamos decidir se coisa X deve acontecer de tal forma",
-        inicio: "17/07/2022",
-        termino: "28/07/2022",
-        status: "ativa",
-        sim: 24,
-        nao: 32,
-        total: 56
-    },
-]
 
-const usuarios = [
-    { id: 1, nome: "Fulano", numero: "991292263" },
-    { id: 2, nome: "Fulano", numero: "981292264" },
-    { id: 3, nome: "Fulano", numero: "981293265" },
-    { id: 4, nome: "Fulano", numero: "991293266" },
-]
+const { votacoes, usuarios, votacao} = require("../data")
 
-const votacao = votacoes[0]
 
 router.get('/login', async (req, res) => {
     res.render('login');
